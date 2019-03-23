b72e03a3c5e088edbc674ebf5439a6617e3afd2e
karimulla1696 <karimulla1696@gmail.com>
2019-03-23 21:32:02 +0530

    heroku

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
+const port = process.env.PORT || 3030;
 
 mongoose.connect('mongodb://localhost/posters');
 const db = mongoose.connection;
 db.on('error', () => console.log('mongodb connection failed'));
 db.once('open', () => console.log('mongodb connection successfull'));
+
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', routes);
 
-
-app.listen(3030,() => console.log('3030  server is ruining'));
+app.listen(port,() => console.log(`${port}  server is ruining`));
